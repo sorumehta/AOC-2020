2d18e9d06319b193fe07e972b213cbe8c6699766
Saurabh Mehta <saurabh@blend.to>
2023-05-07 11:52:53 +0530

    day 8

--- day08/main.py
+++ day08/main.py
@@ -1,6 +1,6 @@
 import argparse
 import os.path
-from typing import List
+from typing import NamedTuple
 
 import pytest
 
@@ -9,27 +9,66 @@ from support import timing
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def parse_input(s: str) -> List[int]:
-    return [int(line) for line in s.splitlines()]
+class Instruction(NamedTuple):
+    op: str
+    arg: int
+
+
+def parse_input(s: str):
+    code: list[Instruction] = []
+    for line in s.splitlines():
+        opc, n_s = line.split()
+        n = int(n_s)
+        code.append(Instruction(opc, n))
+    return code
+
+def compute_code(code:  list[Instruction]):
+    visited: set[int] = set()
+    acc = 0
+    ptr = 0
+    while (ptr not in visited) and (ptr < len(code)):
+        visited.add(ptr)
+        inst = code[ptr]
+        if inst.op == 'nop':
+            ptr += 1
+        elif inst.op == 'acc':
+            acc += inst.arg
+            ptr += 1
+        elif inst.op == 'jmp':
+            ptr += inst.arg
+    return acc, ptr
+
 
 def compute(s: str) -> int:
-    numbers = parse_input(s)
-    for n in numbers:
-        pass
-
-    # TODO: implement solution here!
+    code = parse_input(s)
+    flip = {'nop': 'jmp', 'jmp': 'nop'}
+    for idx, inst in enumerate(code):
+        if inst.op in list(flip.keys()):
+            new_code = code[:]
+            new_code[idx] = Instruction(flip[inst.op], inst.arg)
+            acc, ptr = compute_code(new_code)
+            if ptr >= len(code):
+                print(f"changing {inst.op} to {flip[inst.op]} at idx {idx} worked")
+                return acc
     return 0
 
-
 INPUT_S = '''\
-
+nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6
 '''
 
 
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
-        (INPUT_S, 1),
+        (INPUT_S, 8),
     ),
 )
 def test(input_s: str, expected: int) -> None:
